47b9e9fe9075b15e545972b7fec386e0b4fd82c0
AnasBenba <anasbenba123@gmail.com>
2023-12-19 11:37:51 +0100

    add task 9

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 TypeStorage = getenv("HBNB_TYPE_STORAGE")
@@ -22,6 +23,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref="place", cascade="all,delete")
     else:
         city_id = ""
         user_id = ""
@@ -34,3 +36,16 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+    
+    @property
+    def reviews(self):
+        """returns the list of Review instances
+        with place_id equals to the current Place.id"""
+        from models.review import Review
+        from models.__init__ import storage
+        my_list = []
+        Reviews = storage.all(Review)
+        for rev in Reviews.value():
+            if rev.place_id == self.id:
+                my_list.append(rev)
+        return my_list
--- models/review.py
+++ models/review.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.sql.schema import ForeignKey
+from os import getenv
+
+TypeStorage = getenv("HBNB_TYPE_STORAGE")
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    if TypeStorage == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'),nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
