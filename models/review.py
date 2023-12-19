045467f5986a3764072e4bb2c38cbb099b2e81e7
med.wf <med.wf95@gmail.com>
2023-12-19 11:52:16 +0100

    add inherice Review from Base

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.sql.schema import ForeignKey
 from os import getenv
@@ -8,12 +8,12 @@ from os import getenv
 TypeStorage = getenv("HBNB_TYPE_STORAGE")
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     if TypeStorage == 'db':
         text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'),nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     else:
         place_id = ""
