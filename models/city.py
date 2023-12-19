2484e329506dfe54e967eb1dce8a615c79943cf7
AnasBenba <anasbenba123@gmail.com>
2023-12-19 09:47:21 +0100

    debbuging code task 8

--- models/city.py
+++ models/city.py
@@ -15,7 +15,7 @@ class City(BaseModel, Base):
     if TypeStorage == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", cascade="all,delete", backref="cities")
+        places = relationship("Place", backref="cities", cascade="all,delete")
     else:
         name = ""
         state_id = ""
