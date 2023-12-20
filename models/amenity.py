167753bcef2cb66bac0b4a611f465c72c5500398
med.wf <med.wf95@gmail.com>
2023-12-20 15:25:18 +0100

    fix relationships between amenity and place

--- models/amenity.py
+++ models/amenity.py
@@ -11,6 +11,7 @@ class Amenity(BaseModel, Base):
     if ValueEnv == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity")
+        place_amenities = relationship(
+            "Place", secondary="place_amenity", back_populates="amenities")
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -33,7 +33,8 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref="place", cascade="all,delete")
-        amenities = relationship('Amenity', secondary=place_amenity,
+        amenities = relationship('Amenity', secondary="place_amenity",
+                                 back_populates="place_amenities",
                                  viewonly=False)
     else:
         city_id = ""
