44ccf22884fc280410ab74b536bd59cb3bec6558
AnasBenba <anasbenba123@gmail.com>
2023-12-20 19:24:28 +0100

    undo changes

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,7 @@ ValueEnv = getenv('HBNB_TYPE_STORAGE')
 if ValueEnv == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
