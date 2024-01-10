c068505613747b93f97ee380f6343fe2f264be0f
med.wf <med.wf95@gmail.com>
2024-01-10 22:30:44 +0100

    add task 1

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,1 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models import ValueEnv
-
-
-class Amenity(BaseModel, Base):
-    """class Amenity"""
-    if ValueEnv == "db":
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place", secondary="place_amenity", back_populates="amenities")
-    else:
-        name = ""
