da8d402df6c38a7fa15fdb604c45c77fdfdcd6a3
med.wf <med.wf95@gmail.com>
2024-01-26 21:42:22 +0100

    correct task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -59,6 +59,7 @@ def number_template(n):
     """
     return render_template('5-number.html', n=n)
 
+
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """this methods return template with variable result
